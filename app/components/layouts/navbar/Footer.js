40759fca2597814d9b400911176da8a025beb319
Nuttasan <nuttasanz@gmail.com>
2023-11-29 14:33:56 +0700

    Fix: Language Box

--- app/components/layouts/navbar/Footer.js
+++ app/components/layouts/navbar/Footer.js
@@ -2,11 +2,19 @@
 import Link from "next/link";
 import { FooterRegisterButton } from "../../utils/Buttons/PrimaryButton";
 import "./styles/footerStyle.css";
-import { useState } from "react";
+import React, { useState } from "react";
 
 const Footer = () => {
   const [language, setLanguage] = useState("th");
   const [languageBox, setLanguageBox] = useState(false);
+  const languages = [
+    {
+      code: "en",
+      name: "English, USA",
+      flag: "/images/CountryImages/enUS.png",
+    },
+    { code: "th", name: "ไทย", flag: "/images/CountryImages/thTh.png" },
+  ];
   return (
     <>
       {/* DESKTOP RESPONSIVE */}
@@ -17,74 +25,44 @@ const Footer = () => {
               className="relative my-6 flex items-center gap-2"
               onClick={() => setLanguageBox(!languageBox)}
             >
-              {language === "th" ? (
-                <>
-                  <img
-                    src="/images/CountryImages/thTh.png"
-                    className="max-w-[24px] max-h-[16px]"
-                  />
-                  <p className="text-white">ไทย</p>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="#fff"
-                    className="max-w-[15px] max-h-[10px]"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                    />
-                  </svg>
-                </>
-              ) : (
-                <>
-                  <img
-                    src="/images/CountryImages/enUS.png"
-                    className="max-w-[24px] max-h-[16px]"
-                  />
-                  <p className="text-white">English, USA</p>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="#fff"
-                    className="max-w-[15px] max-h-[10px]"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                    />
-                  </svg>
-                </>
-              )}
+              {languages.map(({ code, name, flag }) => (
+                <React.Fragment key={code}>
+                  {language === code ? (
+                    <>
+                      <img src={flag} className="max-w-[24px] max-h-[16px]" />
+                      <p className="text-white">{name}</p>
+                    </>
+                  ) : null}
+                </React.Fragment>
+              ))}
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="#fff"
+                className="max-w-[15px] max-h-[10px]"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                />
+              </svg>
 
               {languageBox && (
                 <div className="absolute inset-x-0 bottom-[100%] bg-white max-w-[150px] rounded-md mb-2">
-                  <div
-                    className="flex items-center gap-2 p-2 hover:bg-gray-300 rounded"
-                    onClick={() => setLanguage("en")}
-                  >
-                    <img
-                      src="/images/CountryImages/enUS.png"
-                      className="max-w-[24px] max-h-[16px]"
-                    />
-                    <p className="text-black text-[14px]">English, USA</p>
-                  </div>
-                  <div
-                    className="flex items-center gap-2 p-2 hover:bg-gray-300 rounded"
-                    onClick={() => setLanguage("th")}
-                  >
-                    <img
-                      src="/images/CountryImages/thTh.png"
-                      className="max-w-[24px] max-h-[16px]"
-                    />
-                    <p className="text-black text-[14px]">ไทย</p>
-                  </div>
+                  {languages.map(({ code, name, flag }) => (
+                    <div
+                      key={code}
+                      className="flex items-center gap-2 p-2 hover:bg-gray-300 rounded"
+                      onClick={() => setLanguage(code)}
+                    >
+                      <img src={flag} className="max-w-[24px] max-h-[16px]" />
+                      <p className="text-black text-[14px]">{name}</p>
+                    </div>
+                  ))}
                 </div>
               )}
             </button>
